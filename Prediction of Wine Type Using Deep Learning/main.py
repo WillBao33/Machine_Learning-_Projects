6bc69ff182b7e652a40b957a3c60eabc65e50190
William Bao <zhibinwilliam@gmail.com>
2024-04-17 13:29:36 -0400

    Added eval code

--- Prediction of Wine Type Using Deep Learning/main.py
+++ Prediction of Wine Type Using Deep Learning/main.py
@@ -29,8 +29,7 @@ loss_fn = nn.BCELoss()
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 X_train = torch.tensor(X_train.values, dtype=torch.float32)
-y_train = torch.tensor(y_train, dtype=torch.float32)
-y_train = y_train.view(-1, 1)
+y_train = torch.tensor(y_train, dtype=torch.float32).view(-1, 1)
 train_dataset = torch.utils.data.TensorDataset(X_train, y_train)
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=1, shuffle=True)
 
@@ -38,12 +37,43 @@ epochs = 3
 model.to(device)
 for epoch in range(epochs):
     model.train()
+    total_correct = 0
+    total_samples = 0
     for inputs, labels in train_loader:
         inputs, labels = inputs.to(device), labels.to(device)
         y_pred = model(inputs)
+
+        # calculate the accuracy
+        predictions = (y_pred > 0.5).float()
+        correct = (predictions == labels).sum().item()
+        total_correct += correct
+        total_samples += labels.size(0)
+
+        # calculate the loss
         loss = loss_fn(y_pred, labels)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-    print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}')
+    accuracy = total_correct / total_samples
+
+    print(f'Epoch [{epoch+1}/{epochs}], Loss: {loss.item():.4f}, Accuracy: {accuracy:.4f}')
+
+X_test = torch.tensor(X_test.values, dtype=torch.float32)
+y_test = torch.tensor(y_test, dtype=torch.float32).view(-1, 1)
+test_dataset = torch.utils.data.TensorDataset(X_test, y_test)
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset, batch_size=1, shuffle=False)
+model.eval()
+test_correct = 0
+test_samples = 0
+with torch.no_grad():
+    for inputs, labels in test_loader:
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        test_pred = model(inputs)
+        test_predictions = (test_pred > 0.5).float()
+        test_correct += (test_predictions == labels).sum().item()
+        test_samples += labels.size(0)
+
+accuracy = test_correct / test_samples
+print(f'Test accuracy: {accuracy: .4f}')
